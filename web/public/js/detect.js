5911e2df44b023c18958566748cdfcf01f41984d
IkezoeMakoto <150283t@gmail.com>
2018-06-08 00:22:02 +0900

    feat: ua-detector (https://codepen.io/zoe3/pen/NYedxd) を移植した

---
+++ web/public/js/detect.js
@@ -0,0 +1,7 @@
+$('.js-ua-detect').on('click', function () {
+    var ua = $('.js-ua').val();
+    var info = platform.parse(ua);
+    $('.js-os').text(info.os);
+    $('.js-browser').text(info.name);
+    $('.js-browser-version').text(info.version);
+});
