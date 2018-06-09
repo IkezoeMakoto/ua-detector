dded71730a645cf93de198c93e4ae1731938b236
IkezoeMakoto <150283t@gmail.com>
2018-06-09 14:26:10 +0900

    fix: ua判定のJSが動いてなかったのを修正

--- web/public/js/detect.js
+++ web/public/js/detect.js
@@ -1,7 +1,9 @@
-$('.js-ua-detect').on('click', function () {
-    var ua = $('.js-ua').val();
-    var info = platform.parse(ua);
-    $('.js-os').text(info.os);
-    $('.js-browser').text(info.name);
-    $('.js-browser-version').text(info.version);
+$(function() {
+    $('.js-ua-detect').on('click', function () {
+        var ua = $('.js-ua').val();
+        var info = platform.parse(ua);
+        $('.js-os').text(info.os);
+        $('.js-browser').text(info.name);
+        $('.js-browser-version').text(info.version);
+    });
 });
