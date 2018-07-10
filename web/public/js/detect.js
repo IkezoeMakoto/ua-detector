696379f83e749a561beb06f94adf1fcee19b65d8
IkezoeMakoto <150283t@gmail.com>
2018-07-10 20:32:35 +0900

    feat: enter押下時に判定させる

--- web/public/js/detect.js
+++ web/public/js/detect.js
@@ -1,9 +1,17 @@
 $(function() {
-    $('.js-ua-detect').on('click', function () {
+    $('.js-ua-detect').on('click', () => {
         var ua = $('.js-ua').val();
         var info = platform.parse(ua);
         $('.js-os').text(info.os);
         $('.js-browser').text(info.name);
         $('.js-browser-version').text(info.version);
+    });
+
+    // Enter押下時に判定させる
+    $('.js-ua').on("keydown", (e) => {
+        if ((e.which && e.which === 13) || (e.keyCode && e.keyCode === 13)) {
+            $('.js-ua-detect').click();
+            return false;
+        }
     });
 });
